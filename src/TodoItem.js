3c8107cb42669dd892ddaa28269e2bddd40e70ba
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2024-06-19 04:57:20 -0700

    Dynamic classes for TodoItem completed  and hotfix for index.css min-width for min-height

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,12 +3,10 @@ import './TodoItem.css'
 function TodoItem(props) {
     return (
       <li className="TodoItem">
-        <span className="Icon Icon-check
-        Icon-check--active">
+        <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>
             V
         </span>
-        <p className="TodoItem-p
-        TodoItem-p--complete">
+        <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
             {props.text}
         </p>
         <span className="Icon Icon-delete">
