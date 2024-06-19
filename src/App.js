e3ed61f87e1897d9528d21c34c202a3675fd7999
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2024-06-19 03:02:18 -0700

    Initial setup

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
     </div>
+  );
+}
+
+function TodoItem() {
+  return (
+    <li>
+      <span>V</span>
+      <p>Llorar con la llorona</p>
+      <span>X</span>
+    </li>
   );
 }
 
