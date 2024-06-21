ae86bcbcb595d5b748363d8de7aef5c1db34357d
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2024-06-21 02:26:30 -0700

    onChange onClick events

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -3,8 +3,14 @@ import './TodoSearch.css'
 function TodoSearch() {
     return (
       <input
-      placeholder="Cut off onion"
+      placeholder="Assassin's Creed..."
       className='TodoSearch'
+      onChange={(event) => {
+        console.log('You have written in TodoSearch');
+        console.log(event);
+        console.log(event.target);
+        console.log(event.target.value);
+      }}
       />
     );
   }
